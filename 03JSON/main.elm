c60ae28b8ba791b645b6eb7e91dbfb42cfe56152
Alexander Nikolaev <ni-ko-la@mail.ru>
2017-03-13 16:26:52 +0300

    03 Parsing JSON

---
+++ 03JSON/main.elm
@@ -0,0 +1,111 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline exposing (decode, required, optional)
+
+
+type alias Response =
+    { id : Int
+    , joke : String
+    , categories : List String
+    }
+
+
+
+-- model
+
+
+type alias Model =
+    String
+
+
+initModel : Model
+initModel =
+    "Finding a joke..."
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, randomJoke )
+
+
+responseDecoder : Decoder Response
+responseDecoder =
+    decode Response
+        |> required "id" int
+        |> required "joke" string
+        |> optional "categories" (list string) []
+        |> at [ "value" ]
+
+
+randomJoke : Cmd Msg
+randomJoke =
+    let
+        url =
+            "http://api.icndb.com/jokes/random"
+
+        request =
+            -- Http.getString url
+            Http.get url responseDecoder
+
+        cmd =
+            Http.send Joke request
+    in
+        cmd
+
+
+
+-- update
+
+
+type Msg
+    = Joke (Result Http.Error Response)
+    | Reload
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Joke (Ok response) ->
+            ( toString (response.id) ++ " " ++ response.joke, Cmd.none )
+
+        Joke (Err err) ->
+            ( (toString err), Cmd.none )
+
+        Reload ->
+            ( model, randomJoke )
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Reload ] [ text "Fetch a Joke" ]
+        , br [] []
+        , text model
+        ]
+
+
+
+-- subscription
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
